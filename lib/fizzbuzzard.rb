b8ecfd5be5c3ead1742be4ffdf6ed56490aafd5a
Russell <russell@russelldunphy.com>
2013-04-24 19:37:12 +0100

    Remove float stuff cos it dun't really work

--- lib/fizzbuzzard.rb
+++ lib/fizzbuzzard.rb
@@ -8,14 +8,3 @@ class Fixnum
     else to_n end
   end
 end
-
-class Float
-  alias_method :to_n, :to_s
-
-  def to_s
-    if self % 3 == 0 and self % 5 == 0 then "FIZZBUZZ"
-    elsif self % 3 == 0 then "FIZZ"
-    elsif self % 5 == 0 then "BUZZ"
-    else to_n end
-  end
-end
